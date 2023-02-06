84dd59c2f064fa4b2700be27c14640f91bc240de
FaisalMuhammed <faisal707974@gmail.com>
2023-02-06 17:51:58 +0530

    nodemon removed

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,11 @@ import { favInsight, getInsights, listInsights, removeInsight } from '../control
 
 const router = express.Router()
 
-router.route('/').get((req,res)=>{
-    res.json({
-        message : 'Your growth site backend is working'
-    })
+router.route('/').get((req, res) => {
+    res.send(<h1>Growth.cx</h1>)
 })
 router.route('/getInsights').post(getInsights)
-router.route('/listInsights').get(listInsights) 
+router.route('/listInsights').get(listInsights)
 router.route('/removeInsights/:id').delete(removeInsight)
 router.route('/FavInsights/:id').patch(favInsight)
 
